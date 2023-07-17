bf2157f0394a694261972e064f48faf79690b3fb
Dina <alexandra.dina40@yahoo.com>
2023-07-17 14:14:27 +0300

    a

---
+++ Question.py
@@ -0,0 +1,4 @@
+class Question:
+    def __init__(self,prompt,answer): #these are the atribute of Question : prompt, answer
+        self.prompt=prompt # in this 2 lines , the atributes take the values
+        self.answer=answer
